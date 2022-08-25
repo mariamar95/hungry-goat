3acb9f004c4f6383bd42b2579ccd351dc58972c4
Maria <mariamar_@hotmail.gr>
2022-08-25 11:50:00 +0000

    create hungrygoat db and Users table

---
+++ hungrygoat/models.py
@@ -0,0 +1,14 @@
+from hungrygoat import db
+
+
+class Users(db.Model):
+    """ schema for the Users model """
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(260), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.user_name
--- hungrygoat/routes.py
+++ hungrygoat/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from hungrygoat import app, db
+from hungrygoat.models import Users
 
 
 @app.route("/")
