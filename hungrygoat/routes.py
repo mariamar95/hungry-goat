dd367196e5660115d364761786e7eab1741c66be
Maria <mariamar_@hotmail.gr>
2022-08-27 11:39:03 +0000

    add code that adds new user on mongoDB

--- hungrygoat/routes.py
+++ hungrygoat/routes.py
@@ -11,7 +11,8 @@ def home():
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 @app.route("/add_recipe", methods=["GET", "POST"])
@@ -39,6 +40,20 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        # query postgress db to get the id of the user
+        # saves the user_id into a variable
+        new_user = Users.query.get_or_404(user.id)
+
+        # add user to mongoDB
+        user_profile = {
+            "user_id": str(new_user.id),
+            "fave_recipes": [],
+            "my_recipes": []
+        }
+
+        mongo.db.users.insert_one(user_profile)
+
+        # puts user into 'session' cookie
         session["user"] = request.form.get("user_name")
         flash("Registration successful!")
         return redirect(url_for("register", username=session["user"]))
